5c3916325119c04b4dbae640a5ed184f3d59611a
Yuxing Wu <allan.wu6@gmail.com>
2020-07-07 21:28:04 +1000

    Setup the clock hands rotation code

--- Project Analog Clock/script.js
+++ Project Analog Clock/script.js
@@ -1,3 +1,17 @@
 const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
+
+// degrees of the hands on the clock
+let hrPosition;
+let minPosition;
+let secPosition;
+
+// fetch the current hour, minute and second
+var date = new Date();
+console.log(date);
+
+// rotate the hands corresponding to the respective hr/min/sec
+HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
